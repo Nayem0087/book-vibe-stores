d3c635ffc15eeba9b45e20fa5fc2dd0c26a5750b
Nayem Ahmed <nayemk0087@gmail.com>
2026-04-07 13:11:36 +0600

    added my card review section in my interface

--- src/components/Ui/BookCard.jsx
+++ src/components/Ui/BookCard.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import { FaRegStar } from 'react-icons/fa';
+import { Link } from 'react-router';
 
 const BookCard = ({ book }) => {
     return (
-        <div>
-            <div className="card bg-base-100 shadow-sm">
-                <figure className='p-10'>
-                    <img className='rounded-xl h-60'
-                        src={book.image}
-                        alt={book.bookName} />
-                </figure>
-                <div className="card-body">
-                    <div className='flex items-center gap-4'>
-                        {book.tags.map((tag, ind) => (
-                            <div key={ind} className="badge text-green-500 bg-green-200font-bold">{tag}</div>
-                        ))}
-                    </div>
-                    <h2 className='font-bold text-2xl'>{book.bookName}</h2>
-                    <h2 className="card-title">
-                        {book.title}
-                    </h2>
-                    <p className='font-semibold text-lg'>{book.author}</p>
-                    <div className="card-actions justify-between border-t border-dashed border-gray-300 pt-4 text-lg font-semibold">
-                        <div className="">{book.category}</div>
-                        <div className="flex items-center gap-2">{book.rating} <FaRegStar /></div>
-                    </div>
+        <Link to={`/bookDetails/${book.bookId}`}
+         className="card bg-base-100 shadow-sm">
+            <figure className='p-10'>
+                <img className='rounded-xl h-60'
+                    src={book.image}
+                    alt={book.bookName} />
+            </figure>
+            <div className="card-body">
+                <div className='flex items-center gap-4'>
+                    {book.tags.map((tag, ind) => (
+                        <div key={ind} className="badge text-green-500 bg-green-200font-bold">{tag}</div>
+                    ))}
+                </div>
+                <h2 className='font-bold text-2xl'>{book.bookName}</h2>
+                <h2 className="card-title">
+                    {book.title}
+                </h2>
+                <p className='font-semibold text-lg'>{book.author}</p>
+                <div className="card-actions justify-between border-t border-dashed border-gray-300 pt-4 text-lg font-semibold">
+                    <div className="">{book.category}</div>
+                    <div className="flex items-center gap-2">{book.rating} <FaRegStar /></div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
---
+++ src/pages/BookDetails/BookDetails.jsx
@@ -0,0 +1,59 @@
+import React, { use } from 'react';
+import { useLoaderData, useParams } from 'react-router';
+
+const booksPromise = fetch('/booksData.json')
+    .then((res) => res.json());
+
+const BookDetails = () => {
+
+    const { booksId: bookParamsId } = useParams();
+    console.log(bookParamsId, 'book id');
+
+    // const books = use(booksPromise);
+
+    const books = useLoaderData();
+    console.log(books, 'books');
+
+    const expectedBook = books.find((book) => book.booksId == bookParamsId);
+    console.log(expectedBook, 'expectedBook');
+    const {bookId, bookName, author, image, review, totalPages, rating, category, tags, publisher, yearOfPublishing} = expectedBook;
+
+    return (
+        <div className="card lg:card-side bg-base-100 shadow-sm">
+            <figure>
+                <img
+                    src={image}
+                    alt="Album" className='max-h-100'/>
+            </figure>
+            <div className="card-body space-y-3">
+                <h2 className="card-title text-2xl">{bookName}</h2>
+                <h2 className='card-title'>By: {author}</h2>
+                <p className='py-2 border-y'>{category}</p>
+                <p>Review: {review}</p>
+                <div className='flex items-center gap-4'>
+                    {tags.map((tag, ind) => (
+                        <div key={ind} className="badge text-green-500 bg-green-200font-bold">{tag}</div>
+                    ))}
+                </div>
+                <div className='card-actions border-t space-y-3'>
+                      <div className='flex items-center justify-center gap-6'>
+                        <span>Number of pages</span> <span>{totalPages}</span>
+                    </div>
+                    <div className='flex items-center justify-center gap-6'>
+                        <span>Publisher:</span> <span>{publisher}</span>
+                    </div>
+                    <div className='flex items-center justify-center gap-6'>
+                        <span>YearOfPublishing</span> <span>{yearOfPublishing}</span>
+                    </div>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <button className="btn ">Read</button>
+                    <button className="btn btn-primary">WishList</button>
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default BookDetails;
